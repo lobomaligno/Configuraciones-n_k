d27a930219cfe0071af207ac68b085a6c3b6c2ad
Victor Andres Hernandez P <andy1987723@hotmail.com>
2020-01-03 17:53:49 -0600

    paralelisacion

--- ciclosolucion1.sh
+++ ciclosolucion1.sh
@@ -4,9 +4,10 @@ echo "Paso"
 let start=`date +%s`
 while [ 71 -ge $i ]
 do
-    ./paso $i
+    ./paso $i &
     let i=$i+1
 done
+wait
 let end=`date +%s`
 let runtime=$end-$start
 echo "Tardo: $runtime"
