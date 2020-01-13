cf312ecb8c8a86487b25c4d6f89f6a83af9b2e19
Victor Andres Hernandez P <andy1987723@hotmail.com>
2020-01-13 10:17:22 -0600

    Agregar libreria nueva 9

--- SRHpaso.cpp
+++ SRHpaso.cpp
@@ -12,69 +12,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using namespace std;
-unsigned long obtenerhash(graph *g, int tam){
-    hash<string> h;
-    string Hash=string((char*)g, tam*sizeof(graph));
-    return h(Hash);
-}
+#include "SRHfunciones.h"
 #include "naututil.h"
-struct nodo{
-    unsigned long hash;
-    int *grafica;
-    struct nodo *menores;
-    struct nodo *mayores;
-};
-int compararhash(unsigned long hash1, unsigned long hash2){
-    if (hash1 < hash2)
-        return MENOR;
-    if (hash1 > hash2)
-        return MAYOR;
-    return IGUAL;
-}
-int buscarhash(struct nodo *pi,unsigned long hash, struct nodo **padre, int *comparacion){
-    while (pi!=NULL) {
-        *padre=pi;
-        *comparacion=compararhash(hash, pi->hash);
-        if (*comparacion==IGUAL)
-            return ENCONTRADO;
-        if (*comparacion==MAYOR)
-            pi=pi->mayores;
-        else if (*comparacion==MENOR)
-            pi=pi->menores;
-    }
-    return NO_ENCONTRADO;
-}
-int encontrar_o_agregar(struct nodo **raiz, unsigned long hash, int *grafica, int tgrafica){
-    struct nodo *padre;
-    int comparacion;
-    if(buscarhash(*raiz, hash, &padre, &comparacion)==NO_ENCONTRADO){
-        int k;
-        struct nodo *N= (struct nodo *)malloc(sizeof(struct nodo));
-        if (N==NULL)
-            return 2;
-        N->hash=hash;
-        N->grafica=(int *)malloc(sizeof(int)*tgrafica);
-        if (N->grafica==NULL){
-            free(N);
-            return 2;
-        }
-        N->menores=NULL;
-        N->mayores=NULL;
-        for (k = 0; k < tgrafica; ++k)
-            N->grafica[k]=grafica[k];
-        if (*raiz==NULL)
-            *raiz=N;
-        else{
-            if (comparacion==MAYOR)
-                padre->mayores=N;
-            else
-                padre->menores=N;
-        }
-        return NO_ENCONTRADO;
-    }
-    return ENCONTRADO;
-}
 void Borrar(struct nodo *pi){
     if(pi!=NULL){
         Borrar(pi->mayores);
@@ -83,66 +22,10 @@ void Borrar(struct nodo *pi){
         free(pi);
     }
 }
-void imprimirlineas(int nvertices, int tlinea, int *lineas, int tam){
-    int i, j, nencontrados;
-    for (i=0; i<tam; ++i) {
-        printf("(");
-        for (j=0, nencontrados=0; j<nvertices && nencontrados<tlinea; ++j){
-            if (lineas[i]&(1<<j)){
-                ++nencontrados;
-                printf("%d", j+1);
-                if (nencontrados<tlinea){
-                    printf(" ");
-		}
-            }
-        }
-        printf(")");
-    }
-    printf("\n");
-}
 void registrarlinea(int *lineas, int tam, ofstream *myfile, unsigned long hash){
     for (int i=0; i<tam; ++i)
         *myfile<<lineas[i]<<" ";
     *myfile<<hash<<"\n";
-}
-int buscarpar(int *lineas, int etapa, int a, int b){
-    int i;
-    b=(1<<b);
-    b|=(1<<a);
-    for (i=0; i<etapa; i++)
-        if ((lineas[i]&b)==b)
-            return ENCONTRADO;
-    return NO_ENCONTRADO;
-}
-int buscarvertice(int *lineas, int etapa, int vertice, int tlinea){
-    int i, contador;
-    for (i=0, contador=0; i<etapa; i++)
-        if (lineas[i]&(1<<vertice)){
-            contador++;
-            if (contador==tlinea)
-                return ENCONTRADO;
-        }
-    return NO_ENCONTRADO;
-}
-int validar(int *lineas, int *linea, int k, int vertice, int paso, int tlinea){
-    if(buscarvertice(lineas, paso, vertice, tlinea)==ENCONTRADO)
-        return INVALIDO;
-    for (int i=0; i<k; ++i)
-        if (buscarpar(lineas, paso, linea[i], vertice)==ENCONTRADO)
-            return INVALIDO;
-    return VALIDO;
-}
-void graficar(graph *g, int tgrafica, int *lineas, int terminado, int nvertices, int tlinea){
-    int i, j, k;
-    EMPTYGRAPH(g,1,tgrafica);
-    for (i=0; i<terminado; i++)
-        for (j=0; j<nvertices; ++j)
-            if (lineas[i]&(1<<j)){
-                ADDONEEDGE(g, j, nvertices+i, 1);
-                for (k=j+1; k<nvertices; ++k)
-                    if (lineas[i]&(1<<k))
-                        ADDONEEDGE(g, j, k, 1);
-            }
 }
 void escribir(struct nodo *pi, int tam, ofstream *myfile){
     if(pi!=NULL){
@@ -166,51 +49,6 @@ void alinear(struct nodo *P, struct nodo **soluciones, int *contar){
         alinear(P->menores, soluciones, contar);
         soluciones[*contar]=P;
         ++(*contar);
-    }
-}
-void llenarlineas(int *lineas, int *lab, int *ptn, int *orbits, int nvertices, int tlinea, int etapa, struct nodo **arbol, int *contar, int salto){
-    unsigned long hash;
-    int k=1, i, e;
-    int linea[tlinea], S[tlinea];
-    graph solucion[2*nvertices], canon[2*nvertices];
-    static DEFAULTOPTIONS_GRAPH(options);
-    options.getcanon = TRUE;
-    options.defaultptn=TRUE;
-    statsblk stats;
-    for (i=0; i<nvertices && (!(lineas[etapa-1]&(1<<i))); ++i);
-    while (buscarvertice(lineas, etapa, i, tlinea)==ENCONTRADO)
-    ++i;
-    linea[0]=i;
-    S[1]=linea[0]+1;
-    while (k>0) {
-        while (S[k]<=nvertices-tlinea+k) {
-            linea[k]=S[k];
-            ++S[k];
-            if (validar(lineas, linea, k, linea[k], etapa, tlinea)==VALIDO) {
-                if (k==tlinea-1){
-                    lineas[etapa]=0;
-                    for (i=0; i<tlinea; ++i)
-                    lineas[etapa]|=(1<<linea[i]);
-                    if (salto==0){
-                        graficar(solucion, nvertices+etapa+1, lineas, etapa+1, nvertices, tlinea);
-                        densenauty(solucion, lab, ptn, orbits, &options, &stats, 1, nvertices+etapa+1, canon);
-                        //hash=hashgraph(canon, 1, nvertices+etapa+1,0);
-                        hash=obtenerhash(canon, nvertices+etapa+1);
-                        e=encontrar_o_agregar(arbol, hash, &lineas[tlinea], nvertices+etapa+1);
-                        if (e!=ENCONTRADO)
-                            ++*contar;
-                    }
-                    else
-                        llenarlineas(lineas, lab, ptn, orbits, nvertices, tlinea, etapa+1, arbol, contar, salto-1);
-                    
-                }
-                else{
-                    ++k;
-                    S[k]=S[k-1];
-                }
-            }
-        }
-        --k;
     }
 }
 int main(int argc, const char * argv[]) {
